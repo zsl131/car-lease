8f7e8a5dfc6875ebe0ef274c1f7b6d91bc355852
zsl131 <398986099@qq.com>
2016-09-05 17:19:19 +0800

    添加了一些文件

--- auth-server/src/main/java/com/ztw/web/TestController.java
+++ auth-server/src/main/java/com/ztw/web/TestController.java
@@ -1,7 +1,23 @@
 package com.ztw.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.time.LocalDateTime;
+
 /**
  * Created by zsl-pc on 2016/9/4.
  */
+@Controller
+@RequestMapping(value="test")
 public class TestController {
+
+    @RequestMapping(value="index", method = RequestMethod.GET)
+    public String index(Model model) {
+        model.addAttribute("now", LocalDateTime.now());
+        return "index";
+    }
+
 }
